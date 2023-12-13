cee0c1272c0a898540e285963158cfebc6aa9ff4
qudeo <korapadug@gmail.com>
2023-12-13 15:52:40 +0900

    북

---
+++ src/ch10_class/ex8/Book.java
@@ -0,0 +1,27 @@
+package ch10_class.ex8;
+
+public class Book {
+    private String bookTitle;
+    private String bookAuthor;
+
+    // bookTitle setter
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    // bookTitle geeter
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    // bookAuthor setter
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    // bookAutor getter
+    public String getBookAuthor() {
+        return bookTitle;
+    }
+
+}
