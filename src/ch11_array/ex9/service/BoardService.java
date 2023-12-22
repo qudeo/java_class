49e3028116bcc888d583ee5ddc4f282d6fb0da16
qudeo <korapadug@gmail.com>
2023-12-22 18:00:07 +0900

    회원제 게시판 만들기 완료

--- src/ch11_array/ex9/service/BoardService.java
+++ src/ch11_array/ex9/service/BoardService.java
@@ -12,36 +12,55 @@ import java.util.Scanner;
 
 public class BoardService {
     BoardRepository boardRepository = new BoardRepository();
+    //보드레포지토리타입의 보드레포지토리 객체를 선언한다 = 새로히 보드레포지토리클래스를 호출한다
+    //보드레포지토리클래스를 보드레포지토리타입의 보드레포지토리객체를 새롭게 선언해서 호출하여 값을 담는다
     CommentRepository commentRepository = new CommentRepository();
+    //코멘트레포지토리타입의 코멘트레포지토리 객체를 선언한다 = 새로히 보드레포지토리클래스를 호출한다
+    //코멘트레포지토리클래스를 코멘트레포지토리타입의 코멘트레포지토리객체를 새롭게 선언해서 호출하여 값을 담는다
     Scanner scanner = new Scanner(System.in);
+    //스캐너 타입의 스캐너 변수는 새롭게 등록한 스캐너랑 같다
 
     public void write() {
+        // 라이트 메소드
         System.out.print("제목 : ");
         String title = scanner.next();
+        // 스트링 타입의 타이틀 변수는 스캐너로 입력한 값과 같다
         System.out.print("내용 : ");
         String text = scanner.next();
+        // 스트링 타입의 텍스트 변수는 스캐너로 입력한 값과 같다
         boolean result = boardRepository.write(title, text);
+        //블린 타입의 리절트 변수는 보드레포객체의 라이트 메소드에 타이틀과 텍스트 매개체를 가지고있는것과 같다
         if (result) {
+            // 만약에 리절트값이라면
             System.out.println("글작성 완료");
         } else {
+            //그게 아니라면
             System.out.println("글작성 실패");
         }
     }
 
     public void list() {
+        //리턴값이 없고 매개변수가 없는 리스트 메소드
         List<BoardDTO> boardDTOList = boardRepository.list();
+        //보드디티오이름을 가진 리스트의 보드디티오리스트 객체는 보드레포객체의 리스트 메소드 와 같다
         System.out.println(boardDTOList);
     }
 
     public void findList() {
+        //리턴이 없고 매개체도 없는 파인드리스트 메소드
         System.out.print("조회할 글의 id를 입력하세요 : ");
         Long findId = scanner.nextLong();
+        //롱타입의 파인드아이디 변수는 롱타입의 스캐너 값과 같다
         BoardDTO boardDTO = boardRepository.findList(findId);
+        //보드디티오 타입을 가진 보드디티오 객체는 보드레포 객체의 파인드리스트 메소드의 파인드리스트에 전달한 파인드아이디 라는 변수 값을 사용하여 돌아온 값과 같다
         int Select = 0;
+        // 인트타입의 셀렉트는 0과 값이 같다
         if (boardDTO != null) {
+            //만약에 보드디티오랑 널이 같지않을때
             System.out.println(boardDTO);
             System.out.println("== 댓글 ==");
             boolean boardcheck = commentRepository.boardcheck(findId);
+            //블린타입의 보드체크 변수는 코멘트레포 객체의 보드체크 메소드의 보드체크에 전달한 파인드아이디 라는 변수 값을 사용하여 돌아온 값과 같다
             List<CommentDTO> commentDTOList = commentRepository.comment();
             if (commentDTOList.size() > 0) {
                 if (boardcheck) {
